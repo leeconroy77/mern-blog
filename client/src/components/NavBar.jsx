5698302b23f6ab5b2de48d4aaad1521d201e6a1d
leeconroy <leeconroy1977@yahoo.co.uk>
2024-02-19 15:37:27 +0000

    installed react and tailwind and create the first template

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { images } from "../constants";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div>
+        <img src={images.Logo} alt="logo" />
+      </div>
+      <div></div>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "../components/MainLayout";
+
+const HomePage = () => {
+  return (
+    <MainLayout>
+      <h1>Homepage AssBlood</h1>
+    </MainLayout>
+  );
+};
+
+export default HomePage;
